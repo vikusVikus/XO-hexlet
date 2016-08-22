f38e7e3be3a7c19016de7c4a268d0523076a34cd
vikusVikus <kuznetsova.vv@gmail.com>
2016-08-22 15:32:52 +0300

    MoverControllerTest  with applyFigure test and AlreadyOccupiedException test

---
+++ tst/io/hexlet/xo/controller/MoveControllerTest.java
@@ -0,0 +1,44 @@
+package io.hexlet.xo.controller;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
+import org.junit.Test;
+
+import java.awt.*;
+import java.awt.image.PackedColorModel;
+
+import static org.junit.Assert.*;
+
+public class MoveControllerTest {
+    @Test
+    public void applyFigureTest() throws Exception {
+        final Field field = new Field();
+
+        final Point point = new Point(1, 1);
+
+        final Figure inputValue = Figure.X;
+
+        final Figure expectedValue = inputValue;
+
+        MoveController.applyFigure(field, point, inputValue);
+
+        final Figure actualValue = field.getFigure(point);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void pointAlreadyOccupiedTest() throws Exception {
+        final Field field = new Field();
+
+        final Point point = new Point(1, 1);
+
+        MoveController.applyFigure(field, point, Figure.X);
+
+        try {
+            MoveController.applyFigure(field, point, Figure.X);
+            fail();
+        } catch (AlreadyOccupiedException e) {}
+    }
+}
