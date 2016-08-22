a6ca4e39dd890d588092b59bb7befd3e7137f663
vikusVikus <kuznetsova.vv@gmail.com>
2016-08-22 18:11:44 +0300

    CheckColumn test methods added + CheckByRowWithTwoFigure removed as redundant

--- tst/io/hexlet/xo/controller/WinnerControllerTest.java
+++ tst/io/hexlet/xo/controller/WinnerControllerTest.java
@@ -85,21 +85,6 @@ public class WinnerControllerTest {
     }
 
     @Test
-    public void checkByRowWithOneFigureInTheField() throws Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(0,0);
-
-        final Figure figure = Figure.X;
-
-        field.setFigure(p1, figure);
-
-        final Figure actualValue = WinnerController.checkByRow(field);
-
-        assertNull(actualValue);
-    }
-
-    @Test
     public void checkByRowWithSeveralFiguresInTheField() throws Exception {
         final Field field = new Field();
 
@@ -125,11 +110,11 @@ public class WinnerControllerTest {
     public void checkByRowWithRowFilled() throws Exception {
         final Field field = new Field();
 
-        final Point p1 = new Point(2,0);
+        final Point p1 = new Point(1,0);
 
-        final Point p2 = new Point(2,1);
+        final Point p2 = new Point(1,1);
 
-        final Point p3 = new Point(2,2);
+        final Point p3 = new Point(1,2);
 
         final Figure inputValue = Figure.X;
 
@@ -144,4 +129,125 @@ public class WinnerControllerTest {
 
         assertEquals(expectedValue, actualValue);
     }
+
+    @Test
+    public void checkFirstEmptyColumn() throws Exception {
+        final Field field = new Field();
+
+        final Figure actualValue = WinnerController.checkOneColumn(0, field);
+
+        assertNull(actualValue);
+    }
+
+
+    @Test
+    public void checkOneColumnWithOneFigure() throws Exception {
+        final Field field = new Field();
+
+        final Point p1 = new Point(1, 2);
+
+        field.setFigure(p1, Figure.X);
+
+        final Figure actualValue = WinnerController.checkOneColumn(2, field);
+
+        assertNull(actualValue);
+    }
+
+    @Test
+    public void checkOneColumnWithThreeFigures() throws Exception {
+        final Field field = new Field();
+
+        final Point p1 = new Point(0,0);
+
+        final Point p2 = new Point(1,0);
+
+        final Point p3 = new Point(2,0);
+
+        field.setFigure(p1, Figure.X);
+        field.setFigure(p2, Figure.O);
+        field.setFigure(p3, Figure.X);
+
+        final Figure actualValue = WinnerController.checkOneColumn(0, field);
+
+        assertNull(actualValue);
+    }
+
+
+    @Test
+    public void checkOneColumnWithThreeSameFigures() throws Exception {
+        final Field field = new Field();
+
+        final Figure inputValue = Figure.X;
+
+        final Figure expectedValue = inputValue;
+
+        final Point p1 = new Point(0,1);
+
+        final Point p2 = new Point(1,1);
+
+        final Point p3 = new Point(2,1);
+
+        field.setFigure(p1, inputValue);
+        field.setFigure(p2, inputValue);
+        field.setFigure(p3, inputValue);
+
+        final Figure actualValue = WinnerController.checkOneColumn(1, field);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void checkByColumnWithEmptyField() throws Exception {
+        final Field field = new Field();
+
+        final Figure actualValue = WinnerController.checkByColumn(field);
+
+        assertNull(actualValue);
+    }
+
+    @Test
+    public void checkByColumnWithSeveralFiguresInTheField() throws Exception {
+        final Field field = new Field();
+
+        final Point p1 = new Point(0,0);
+
+        final Point p2 = new Point(0,2);
+
+        final Point p3 = new Point(1,2);
+
+        final Point p4 = new Point(2,2);
+
+        field.setFigure(p1, Figure.X);
+        field.setFigure(p2, Figure.O);
+        field.setFigure(p3, Figure.X);
+        field.setFigure(p4, Figure.O);
+
+        final Figure actualValue = WinnerController.checkByColumn(field);
+
+        assertNull(actualValue);
+    }
+
+    @Test
+    public void checkByColumnWithRowFilled() throws Exception {
+        final Field field = new Field();
+
+        final Point p1 = new Point(0,1);
+
+        final Point p2 = new Point(1,1);
+
+        final Point p3 = new Point(2,1);
+
+        final Figure inputValue = Figure.X;
+
+        final Figure expectedValue = inputValue;
+
+        field.setFigure(p1, inputValue);
+        field.setFigure(p2, inputValue);
+        field.setFigure(p3, inputValue);
+
+        final Figure actualValue = WinnerController.checkByColumn(field);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
 }
