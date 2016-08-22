c9a79287c018b184b03d44ffbac04d26725cbe29
vikusVikus <kuznetsova.vv@gmail.com>
2016-08-22 15:33:30 +0300

    MoveController minor edits for testing purposes

--- src/io/hexlet/xo/controller/MoveController.java
+++ src/io/hexlet/xo/controller/MoveController.java
@@ -8,7 +8,7 @@ import io.hexlet.xo.model.exceptions.InvalidPointException;
 import java.awt.*;
 
 public class MoveController {
-    public void applyFigure(final Field field, final Point point, final Figure figure)
+    public static void applyFigure(final Field field, final Point point, final Figure figure)
                 throws InvalidPointException, AlreadyOccupiedException{
         if (field.getFigure(point) != null) {
             throw new AlreadyOccupiedException();
