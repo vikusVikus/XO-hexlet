affa81882cb98beb12b2df54e5c635a520fd8fb9
vikusVikus <kuznetsova.vv@gmail.com>
2016-08-22 13:35:35 +0300

    Field tests checks for exceptions added

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,5 +1,6 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
 
 import java.awt.*;
@@ -32,4 +33,62 @@ public class FieldTest {
         assertEquals(inputFigure, actualFigure);
     }
 
+    @Test
+    public void getFigureWhenFigureNotSet() throws Exception {
+        final Field field = new Field();
+
+        final Point inputPoint = new Point(0,0);
+
+        final Figure actualValue = field.getFigure(inputPoint);
+
+        assertNull(actualValue);
+    }
+
+    @Test
+    public void getFigureWhenXIsLessThenZero() throws Exception {
+        final Field field = new Field();
+
+        final Point inputPoint = new Point(-1,0);
+
+        try {
+            final Figure actualValue = field.getFigure(inputPoint);
+            fail();
+        } catch(final InvalidPointException e) { }
+    }
+
+    @Test
+    public void getFigureWhenYIsLessThenZero() throws Exception {
+        final Field field = new Field();
+
+        final Point inputPoint = new Point(0, -1);
+
+        try {
+            final Figure actualValue = field.getFigure(inputPoint);
+            fail();
+        } catch(final InvalidPointException e) {}
+    }
+
+    @Test
+    public void getFigureWhenXIsMoreThenZero() throws Exception {
+        final Field field = new Field();
+
+        final Point inputPoint = new Point(3, 0);
+
+        try {
+            final Figure actualValue = field.getFigure(inputPoint);
+            fail();
+        } catch(final InvalidPointException e) {}
+    }
+
+    @Test
+    public void getFigureWhenYIsMoreThenZero()  throws Exception {
+        final Field field = new Field();
+
+        final Point inputPoint = new Point(0,3);
+
+        try {
+            final Figure actualValue = field.getFigure(inputPoint);
+            fail();
+        } catch(final InvalidPointException e) {}
+    }
 }
