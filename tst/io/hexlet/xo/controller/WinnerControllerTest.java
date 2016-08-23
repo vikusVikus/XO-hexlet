9d79c4ad240d7366b5aa10583b900d3768b2bfcb
vikusVikus <kuznetsova.vv@gmail.com>
2016-08-23 12:51:25 +0300

    WinnerController test refactoring

--- tst/io/hexlet/xo/controller/WinnerControllerTest.java
+++ tst/io/hexlet/xo/controller/WinnerControllerTest.java
@@ -9,401 +9,137 @@ import java.awt.*;
 import static org.junit.Assert.*;
 
 public class WinnerControllerTest {
-    @Test
-    public void checkFirstEmptyRow() throws Exception {
-       final Field field = new Field();
 
-        final Figure actualValue = WinnerController.checkOneRow(0, field);
+    @Test
+    public void getWinnerWithEmptyFieldTest() throws Exception {
+        final Field field = new Field(5);
+
+        final WinnerController wc = new WinnerController();
+
+        final Figure actualValue = wc.getWinner(field);
 
         assertNull(actualValue);
     }
 
     @Test
-    public void checkOneRowWithOneFigure() throws Exception {
-        final Field field = new Field();
+    public void getWinnerWithSeveralRandomFiguresTest() throws Exception {
+        final Field field = new Field(5);
 
-        final Point p1 = new Point(1, 2);
+        final WinnerController wc = new WinnerController();
 
-        field.setFigure(p1, Figure.X);
+        final MoveController mv = new MoveController();
 
-        final Figure actualValue = WinnerController.checkOneRow(1, field);
+        mv.applyFigure(field, new Point(3,4), Figure.X);
+        mv.applyFigure(field, new Point(3,0), Figure.X);
+        mv.applyFigure(field, new Point(1,4), Figure.O);
+        mv.applyFigure(field, new Point(3,2), Figure.X);
+        mv.applyFigure(field, new Point(0,1), Figure.O);
+        mv.applyFigure(field, new Point(1,2), Figure.X);
 
-        assertNull(actualValue);
-    }
-
-    @Test
-    public void checkOneRowWithThreeFigures() throws Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(1,2);
-
-        final Point p2 = new Point(1,1);
-
-        final Point p3 = new Point(1,0);
-
-        field.setFigure(p1, Figure.X);
-        field.setFigure(p2, Figure.O);
-        field.setFigure(p3, Figure.X);
-
-        final Figure actualValue = WinnerController.checkOneRow(1, field);
+        final Figure actualValue = wc.getWinner(field);
 
         assertNull(actualValue);
     }
 
     @Test
-    public void checkOneRowWithThreeSameFigures() throws Exception {
-        final Field field = new Field();
+    public void getWinnerWithRowFilledTest() throws Exception {
+        final Field field = new Field(5);
 
-        final Figure inputValue = Figure.X;
+        final WinnerController wc = new WinnerController();
 
-        final Figure expectedValue = inputValue;
-
-        final Point p1 = new Point(1,2);
-
-        final Point p2 = new Point(1,1);
-
-        final Point p3 = new Point(1,0);
-
-        field.setFigure(p1, inputValue);
-        field.setFigure(p2, inputValue);
-        field.setFigure(p3, inputValue);
-
-        final Figure actualValue = WinnerController.checkOneRow(1, field);
-
-        assertEquals(expectedValue, actualValue);
-    }
-
-    @Test
-    public void checkByRowWithEmptyField() throws Exception {
-        final Field field = new Field();
-
-        final Figure actualValue = WinnerController.checkByRow(field);
-
-        assertNull(actualValue);
-    }
-
-    @Test
-    public void checkByRowWithSeveralFiguresInTheField() throws Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(0,0);
-
-        final Point p2 = new Point(0,2);
-
-        final Point p3 = new Point(1,2);
-
-        final Point p4 = new Point(2,2);
-
-        field.setFigure(p1, Figure.X);
-        field.setFigure(p2, Figure.O);
-        field.setFigure(p3, Figure.X);
-        field.setFigure(p4, Figure.O);
-
-        final Figure actualValue = WinnerController.checkByRow(field);
-
-        assertNull(actualValue);
-    }
-
-    @Test
-    public void checkByRowWithRowFilled() throws Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(1,0);
-
-        final Point p2 = new Point(1,1);
-
-        final Point p3 = new Point(1,2);
-
-        final Figure inputValue = Figure.X;
-
-        final Figure expectedValue = inputValue;
-
-        field.setFigure(p1, inputValue);
-        field.setFigure(p2, inputValue);
-        field.setFigure(p3, inputValue);
-
-
-        final Figure actualValue = WinnerController.checkByRow(field);
-
-        assertEquals(expectedValue, actualValue);
-    }
-
-    @Test
-    public void checkFirstEmptyColumn() throws Exception {
-        final Field field = new Field();
-
-        final Figure actualValue = WinnerController.checkOneColumn(0, field);
-
-        assertNull(actualValue);
-    }
-
-
-    @Test
-    public void checkOneColumnWithOneFigure() throws Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(1, 2);
-
-        field.setFigure(p1, Figure.X);
-
-        final Figure actualValue = WinnerController.checkOneColumn(2, field);
-
-        assertNull(actualValue);
-    }
-
-    @Test
-    public void checkOneColumnWithThreeFigures() throws Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(0,0);
-
-        final Point p2 = new Point(1,0);
-
-        final Point p3 = new Point(2,0);
-
-        field.setFigure(p1, Figure.X);
-        field.setFigure(p2, Figure.O);
-        field.setFigure(p3, Figure.X);
-
-        final Figure actualValue = WinnerController.checkOneColumn(0, field);
-
-        assertNull(actualValue);
-    }
-
-
-    @Test
-    public void checkOneColumnWithThreeSameFigures() throws Exception {
-        final Field field = new Field();
-
-        final Figure inputValue = Figure.X;
-
-        final Figure expectedValue = inputValue;
-
-        final Point p1 = new Point(0,1);
-
-        final Point p2 = new Point(1,1);
-
-        final Point p3 = new Point(2,1);
-
-        field.setFigure(p1, inputValue);
-        field.setFigure(p2, inputValue);
-        field.setFigure(p3, inputValue);
-
-        final Figure actualValue = WinnerController.checkOneColumn(1, field);
-
-        assertEquals(expectedValue, actualValue);
-    }
-
-    @Test
-    public void checkByColumnWithEmptyField() throws Exception {
-        final Field field = new Field();
-
-        final Figure actualValue = WinnerController.checkByColumn(field);
-
-        assertNull(actualValue);
-    }
-
-    @Test
-    public void checkByColumnWithSeveralFiguresInTheField() throws Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(0,0);
-
-        final Point p2 = new Point(0,2);
-
-        final Point p3 = new Point(1,2);
-
-        final Point p4 = new Point(2,2);
-
-        field.setFigure(p1, Figure.X);
-        field.setFigure(p2, Figure.O);
-        field.setFigure(p3, Figure.X);
-        field.setFigure(p4, Figure.O);
-
-        final Figure actualValue = WinnerController.checkByColumn(field);
-
-        assertNull(actualValue);
-    }
-
-    @Test
-    public void checkByColumnWithRowFilled() throws Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(0,1);
-
-        final Point p2 = new Point(1,1);
-
-        final Point p3 = new Point(2,1);
-
-        final Figure inputValue = Figure.X;
-
-        final Figure expectedValue = inputValue;
-
-        field.setFigure(p1, inputValue);
-        field.setFigure(p2, inputValue);
-        field.setFigure(p3, inputValue);
-
-        final Figure actualValue = WinnerController.checkByColumn(field);
-
-        assertEquals(expectedValue, actualValue);
-    }
-
-    @Test
-    public void checkFirstDiagonalWithNoFigures() throws Exception {
-        final Field field = new Field();
-
-        Figure actualValue = WinnerController.checkFirstDiagonal(field);
-
-        assertNull(actualValue);
-    }
-
-    @Test
-    public void checkFirstDiagonalWithThreeRandomFigures() throws Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(0,0);
-
-        final Point p2 = new Point(1,1);
-
-        final Point p3 = new Point(2,2);
-
-        field.setFigure(p1, Figure.X);
-        field.setFigure(p2, Figure.O);
-        field.setFigure(p3, Figure.X);
-
-        Figure actualValue = WinnerController.checkFirstDiagonal(field);
-
-        assertNull(actualValue);
-    }
-
-    @Test
-    public void checkFirstDiagonalWithThreeEqaulFigures() throws Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(0,0);
-
-        final Point p2 = new Point(1,1);
-
-        final Point p3 = new Point(2,2);
-
-        final Figure inputValue = Figure.X;
-
-        final Figure expectedValue = inputValue;
-
-        field.setFigure(p1, Figure.X);
-        field.setFigure(p2, Figure.X);
-        field.setFigure(p3, Figure.X);
-
-        Figure actualValue = WinnerController.checkFirstDiagonal(field);
-
-        assertEquals(expectedValue, actualValue);
-    }
-
-    @Test
-    public void checkSecondDiagonalWithNoFigures() throws Exception {
-        final Field field = new Field();
-
-        Figure actualValue = WinnerController.checkSecondDiagonal(field);
-
-        assertNull(actualValue);
-    }
-
-    @Test
-    public void checkSecondDiagonalWithThreeRandomFigures() throws Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(2,0);
-
-        final Point p2 = new Point(1,1);
-
-        final Point p3 = new Point(0,2);
-
-        field.setFigure(p1, Figure.X);
-        field.setFigure(p2, Figure.O);
-        field.setFigure(p3, Figure.X);
-
-        Figure actualValue = WinnerController.checkSecondDiagonal(field);
-
-        assertNull(actualValue);
-    }
-
-    @Test
-    public void checkSecondDiagonalWithThreeEqaulFigures() throws Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(2,0);
-
-        final Point p2 = new Point(1,1);
-
-        final Point p3 = new Point(0,2);
-
-        final Figure inputValue = Figure.X;
-
-        final Figure expectedValue = inputValue;
-
-        field.setFigure(p1, Figure.X);
-        field.setFigure(p2, Figure.X);
-        field.setFigure(p3, Figure.X);
-
-        Figure actualValue = WinnerController.checkSecondDiagonal(field);
-
-        assertEquals(expectedValue, actualValue);
-    }
-
-    @Test
-    public void getWinnerTestWithEmptyField() throws  Exception {
-        final Field field = new Field();
-
-        Figure actualValue = WinnerController.getWinner(field);
-
-        assertNull(actualValue);
-    }
-
-    @Test
-    public void getWinnerTestWithSeveralFigures() throws  Exception {
-        final Field field = new Field();
-
-        final Point p1 = new Point(0,0);
-
-        final Point p2 = new Point(0,2);
-
-        final Point p3 = new Point(1,0);
-
-        final Point p4 = new Point(2,2);
-
-        field.setFigure(p1, Figure.X);
-        field.setFigure(p2, Figure.O);
-        field.setFigure(p3, Figure.X);
-        field.setFigure(p4, Figure.O);
-
-        Figure actualValue = WinnerController.getWinner(field);
-
-        assertNull(actualValue);
-    }
-
-    @Test
-    public void getWinnerTestWithAWinnerFigure() throws  Exception {
-        final Field field = new Field();
+        final MoveController mv = new MoveController();
 
         final Figure expectedValue = Figure.X;
 
-        final Point p1 = new Point(0,0);
+        mv.applyFigure(field, new Point(4,4), Figure.X);
+        mv.applyFigure(field, new Point(1,4), Figure.O);
+        mv.applyFigure(field, new Point(4,2), Figure.X);
+        mv.applyFigure(field, new Point(4,1), Figure.X);
+        mv.applyFigure(field, new Point(0,0), Figure.O);
+        mv.applyFigure(field, new Point(0,1), Figure.O);
+        mv.applyFigure(field, new Point(4,0), Figure.X);
+        mv.applyFigure(field, new Point(3,3), Figure.O);
+        mv.applyFigure(field, new Point(4,3), Figure.X);
 
-        final Point p2 = new Point(0,2);
+        final Figure actualValue = wc.getWinner(field);
 
-        final Point p3 = new Point(1,1);
+        assertEquals(expectedValue, actualValue);
+    }
 
-        final Point p4 = new Point(1,2);
+    @Test
+    public void getWinnerWithColumnFilledTest() throws Exception {
+        final Field field = new Field(5);
 
-        final Point p5 = new Point(2,2);
+        final WinnerController wc = new WinnerController();
 
-        field.setFigure(p1, Figure.X);
-        field.setFigure(p2, Figure.O);
-        field.setFigure(p3, Figure.X);
-        field.setFigure(p4, Figure.O);
-        field.setFigure(p5, Figure.X);
+        final MoveController mv = new MoveController();
 
-        Figure actualValue = WinnerController.getWinner(field);
+        final Figure expectedValue = Figure.O;
+
+        mv.applyFigure(field, new Point(3,3), Figure.X);
+        mv.applyFigure(field, new Point(0,4), Figure.O);
+        mv.applyFigure(field, new Point(4,2), Figure.X);
+        mv.applyFigure(field, new Point(1,4), Figure.O);
+        mv.applyFigure(field, new Point(0,0), Figure.X);
+        mv.applyFigure(field, new Point(2,4), Figure.O);
+        mv.applyFigure(field, new Point(1,0), Figure.X);
+        mv.applyFigure(field, new Point(3,4), Figure.O);
+        mv.applyFigure(field, new Point(4,3), Figure.X);
+        mv.applyFigure(field, new Point(4,4), Figure.O);
+
+        final Figure actualValue = wc.getWinner(field);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getWinnerWithFirstDiagonalFilledTest() throws Exception {
+        final Field field = new Field(5);
+
+        final WinnerController wc = new WinnerController();
+
+        final MoveController mv = new MoveController();
+
+        final Figure expectedValue = Figure.O;
+
+        mv.applyFigure(field, new Point(3,0), Figure.X);
+        mv.applyFigure(field, new Point(0,0), Figure.O);
+        mv.applyFigure(field, new Point(4,2), Figure.X);
+        mv.applyFigure(field, new Point(1,1), Figure.O);
+        mv.applyFigure(field, new Point(0,1), Figure.X);
+        mv.applyFigure(field, new Point(2,2), Figure.O);
+        mv.applyFigure(field, new Point(1,0), Figure.X);
+        mv.applyFigure(field, new Point(3,3), Figure.O);
+        mv.applyFigure(field, new Point(4,3), Figure.X);
+        mv.applyFigure(field, new Point(4,4), Figure.O);
+
+        final Figure actualValue = wc.getWinner(field);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getWinnerWithSecondDiagonalFilledTest() throws Exception {
+        final Field field = new Field(5);
+
+        final WinnerController wc = new WinnerController();
+
+        final MoveController mv = new MoveController();
+
+        final Figure expectedValue = Figure.X;
+
+        mv.applyFigure(field, new Point(0,4), Figure.X);
+        mv.applyFigure(field, new Point(0,0), Figure.O);
+        mv.applyFigure(field, new Point(1,0), Figure.O);
+        mv.applyFigure(field, new Point(1,3), Figure.X);
+        mv.applyFigure(field, new Point(2,2), Figure.X);
+        mv.applyFigure(field, new Point(2,0), Figure.O);
+        mv.applyFigure(field, new Point(3,1), Figure.X);
+        mv.applyFigure(field, new Point(3,4), Figure.O);
+        mv.applyFigure(field, new Point(4,0), Figure.X);
+        mv.applyFigure(field, new Point(4,2), Figure.O);
+
+        final Figure actualValue = wc.getWinner(field);
 
         assertEquals(expectedValue, actualValue);
     }
