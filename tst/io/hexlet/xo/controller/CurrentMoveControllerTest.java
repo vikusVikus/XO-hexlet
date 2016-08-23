b81494a7fceed9ef04dd6b8abc9ef95e0360369e
vikusVikus <kuznetsova.vv@gmail.com>
2016-08-23 13:28:45 +0300

    CurrentMoveController tests added

---
+++ tst/io/hexlet/xo/controller/CurrentMoveControllerTest.java
@@ -0,0 +1,81 @@
+package io.hexlet.xo.controller;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class CurrentMoveControllerTest {
+    @Test
+    public void currentMoveWithEmptyFieldTest() throws Exception {
+        final Field field = new Field(3);
+
+        final CurrentMoveController cmc = new CurrentMoveController();
+
+        final Figure expectedValue = Figure.X;
+
+        final Figure actualValue = cmc.currentMove(field);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void currentMoveWithOneFieldTest() throws Exception {
+        final Field field = new Field(3);
+
+        final CurrentMoveController cmc = new CurrentMoveController();
+
+        final MoveController mc = new MoveController();
+
+        final Figure expectedValue = Figure.O;
+
+        mc.applyFigure(field, new Point(1,2), Figure.X);
+
+        final Figure actualValue = cmc.currentMove(field);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void currentMoveWithTwoFieldTest() throws Exception {
+        final Field field = new Field(3);
+
+        final CurrentMoveController cmc = new CurrentMoveController();
+
+        final MoveController mc = new MoveController();
+
+        final Figure expectedValue = Figure.X;
+
+        mc.applyFigure(field, new Point(1,2), Figure.X);
+        mc.applyFigure(field, new Point(2,2), Figure.O);
+
+        final Figure actualValue = cmc.currentMove(field);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+
+    @Test
+    public void currentMoveWithFiveFieldTest() throws Exception {
+        final Field field = new Field(3);
+
+        final CurrentMoveController cmc = new CurrentMoveController();
+
+        final MoveController mc = new MoveController();
+
+        final Figure expectedValue = Figure.O;
+
+        mc.applyFigure(field, new Point(1,2), Figure.X);
+        mc.applyFigure(field, new Point(2,2), Figure.O);
+        mc.applyFigure(field, new Point(2,0), Figure.X);
+        mc.applyFigure(field, new Point(0,2), Figure.O);
+        mc.applyFigure(field, new Point(1,1), Figure.X);
+
+        final Figure actualValue = cmc.currentMove(field);
+
+        assertEquals(expectedValue, actualValue);
+    }
+}
