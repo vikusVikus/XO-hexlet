dde5fb0117c5bd4a4462970822dd4ccc5787c22a
vikusVikus <kuznetsova.vv@gmail.com>
2016-08-26 16:21:36 +0300

    View package + consoleView class added

---
+++ src/io/hexlet/xo/view/ConsoleView.java
@@ -0,0 +1,13 @@
+package io.hexlet.xo.view;
+
+import io.hexlet.xo.model.Game;
+
+public class ConsoleView {
+    public void show(final Game game) {
+
+    }
+
+    public void move(final Game game) {
+
+    }
+}
